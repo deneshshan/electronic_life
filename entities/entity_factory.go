8f7d9be7761efc7dfe4ee85a991aa37376e3a9d6
Denesh Shan <denesh.shan@outlook.com>
2017-04-17 23:25:01 +0200

    Seperate rabbitBuilder, entityBuilder and EntityFactory into seperate files

--- entities/entity_factory.go
+++ entities/entity_factory.go
@@ -1,57 +1,7 @@
 package entities
 
-import (
-	"github.com/looplab/fsm"
-)
-
-type EntityType int
-
-const (
-	carrot EntityType = iota
-	rabbit
-)
-
-type entityBuilder struct {
-}
-
-type rabbitBuilder struct {
-	entityBuilder
-}
-
-type EntityBuilder interface {
-	setType(entityType EntityType) EntityBuilder
-	build() actor
-}
-
 func Create(entityType EntityType) actor {
 	instance := &entityBuilder{}
 	entity := instance.setType(entityType).build()
 	return entity
-}
-
-func (eb entityBuilder) setType(entityType EntityType) EntityBuilder {
-	switch entityType {
-	case rabbit:
-		return &rabbitBuilder{}
-	default:
-		panic("EntityBuilder typeOption not recognised")
-	}
-}
-
-func (eb *rabbitBuilder) build() actor {
-	entity := &Rabbit{}
-	entity.FSM = getRabbitFSM(entity)
-	return entity
-}
-
-func getRabbitFSM(sm stateMachine) *fsm.FSM {
-	fsm := fsm.NewFSM(
-		"searching",
-		fsm.Events{},
-		fsm.Callbacks{
-			"enter_state": func(e *fsm.Event) { sm.enterState(e) },
-		},
-	)
-
-	return fsm
 }
